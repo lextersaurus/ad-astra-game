8f8be970ae0095a617bb7e4a3c1cfccdab33e84e
Esther Mesa <estmesher@gmail.com>
2024-02-03 14:38:19 +0000

    player jump added

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,12 @@
 window.onload = () => {
     astronaut
+    document.addEventListener('keydown', moveAstronaut)
     console.log(astronaut)
+}
+
+function moveAstronaut(e) {
+    if ((e.code == 'Space' || e.code == 'ArrowUp') && astronaut.astroTop >= astronautY) {
+        astronaut.jump()
+        astronaut.isJumping = true
+    }
 }
--- scripts/player.js
+++ scripts/player.js
@@ -5,6 +5,32 @@ class Player {
         this.sprite = sprite
         this.sprite.style.top = `${this.astroTop}px`
         this.sprite.style.left = `${this.astroLeft}px`
+        this.movementY = 10
+        this.timer = null
+        this.direction = 1
+        this.isJumping = false
+        this.gravity = 1
+    }
+
+    jump() {
+        this.timer = setInterval(() => {
+            this.astroTop -= this.movementY * this.direction * this.gravity
+            this.sprite.style.top = `${this.astroTop}px`
+            if (this.astroTop <= 30) {
+                this.direction *= -1
+                this.gravity = 1.5
+            }
+            if (this.astroTop >= 188 && this.isJumping){
+                this.stop()
+                this.direction = 1
+                this.gravity = 1
+                this.isJumping = false
+            }
+        }, 30)
+    }
+    
+    stop() {
+        clearInterval(this.timer)
     }
 }
 
@@ -19,3 +45,4 @@ let playerHeight = player.offsetHeight
 let astronautY = boardHeight - playerHeight - floorHeight
 
 const astronaut = new Player(30, astronautY, player)
+console.log(astronaut.astroTop)
